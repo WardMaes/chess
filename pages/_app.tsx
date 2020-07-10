6e203b5b555f8a39dd76a25cf69859e2c49eac5b
Ward <ward@bothrs.com>
2020-07-10 08:14:00 +0200

    Fix typings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,18 +7,14 @@ import { chessMachine } from '../lib/machines/chess'
 import { chessContext as ChessContext } from '../lib/context'
 
 const MyApp: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
-  // eslint-disable-next-line react/prop-types
   Component,
-  // eslint-disable-next-line react/prop-types
   pageProps,
 }) => {
   const modifiedPageProps = { ...pageProps }
 
-  const [state, send, service] = useMachine(chessMachine, { devTools: true })
-  const machine = [state, send, service]
+  const machine = useMachine(chessMachine, { devTools: true })
 
   return (
-    // @ts-ignore
     <ChessContext.Provider value={machine}>
       <Component {...modifiedPageProps} />
     </ChessContext.Provider>
